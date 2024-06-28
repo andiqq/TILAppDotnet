b33ec638a488c4f37de11146705ef33d4d44136d
Dr. Andreas Plagens <andreas@plagens.eu>
2024-06-28 22:31:21 +0200

    continued minimal Api

--- TILAppMinimal/Endpoints/AcronymEndpoints.cs
+++ TILAppMinimal/Endpoints/AcronymEndpoints.cs
@@ -33,14 +33,47 @@ public static class AcronymEndpoints
         group.MapGet("{id}/Categories",
                 async Task<Results<Ok<List<Category.CDto>>, NotFound>> (int id, Context db) =>
                 {
-                    var acronym = await db.Acronym
+                    var acronym = await db.Acronym.AsNoTracking()
                         .Include(a => a.Categories)
                         .FirstOrDefaultAsync(a => a.Id == id);
-                    return acronym != null
+                    return acronym is not null
                         ? TypedResults.Ok(new Category.CDto().List(acronym.Categories.ToList()))
                         : TypedResults.NotFound();
                 })
             .WithName("GetAcronymCategories")
+            .WithOpenApi();
+
+        //GET: minimalapi/Acronym/5/User
+        group.MapGet("{id}/User",
+                async Task<Results<Ok<User.Public>, NotFound>> (int id, Context db) =>
+                {
+                    if (db.Acronym is null) return TypedResults.NotFound();
+                    var acronym = await db.Acronym.FindAsync(id);
+
+                    if (acronym is null) return TypedResults.NotFound();
+                    var user = await db.User.FindAsync(acronym.UserId);
+
+                    return user is null
+                        ? TypedResults.NotFound()
+                        : TypedResults.Ok(new User.Public(user));
+                })
+            .WithName("GetAcronymUser")
+            .WithOpenApi();
+
+        //GET: minimalapi/Acronym/search?Term=OMG
+        group.MapGet("search",
+            async Task<Results<Ok<List<Acronym.Dto>>, NotFound>> (string term, Context db) =>
+            {
+                var acronyms = await db.Acronym.AsNoTracking()
+                    .Where(a => a.Short.Contains(term) ||
+                                a.Long.Contains(term)
+                    ).ToListAsync();
+
+                return acronyms is null || acronyms.Count == 0
+                    ? TypedResults.NotFound()
+                    : TypedResults.Ok(new Acronym.Dto().List(acronyms));
+            })
+            .WithName("SearchForAcronymWithTerm")
             .WithOpenApi();
     }
 }
