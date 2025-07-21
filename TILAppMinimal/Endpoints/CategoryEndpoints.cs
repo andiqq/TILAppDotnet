dddf957601c7f72ead6199ee3bd700aaa614bc49
Dr. Andreas Plagens <andreas@plagens.eu>
2025-07-21 22:01:41 +0200

    Added CategoryEndpoints

---
+++ TILAppMinimal/Endpoints/CategoryEndpoints.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.OpenApi;
+using TILApp.Data;
+using TILApp.Models;
+namespace TILAppMinimal.Endpoints;
+
+public static class CategoryEndpoints
+{
+    public static void MapCategoryEndpoints (this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/api/Category").WithTags(nameof(Category));
+
+        group.MapGet("/", async (Context db) =>
+        {
+            return await db.Category.ToListAsync();
+        })
+        .WithName("GetAllCategories")
+        .WithOpenApi();
+
+        group.MapGet("/{id}", async Task<Results<Ok<Category>, NotFound>> (int id, Context db) =>
+        {
+            return await db.Category.AsNoTracking()
+                .FirstOrDefaultAsync(model => model.Id == id)
+                is Category model
+                    ? TypedResults.Ok(model)
+                    : TypedResults.NotFound();
+        })
+        .WithName("GetCategoryById")
+        .WithOpenApi();
+
+        group.MapPut("/{id}", async Task<Results<Ok, NotFound>> (int id, Category category, Context db) =>
+        {
+            var affected = await db.Category
+                .Where(model => model.Id == id)
+                .ExecuteUpdateAsync(setters => setters
+                    .SetProperty(m => m.Id, category.Id)
+                    .SetProperty(m => m.Name, category.Name)
+                    );
+            return affected == 1 ? TypedResults.Ok() : TypedResults.NotFound();
+        })
+        .WithName("UpdateCategory")
+        .WithOpenApi();
+
+        group.MapPost("/", async (Category category, Context db) =>
+        {
+            db.Category.Add(category);
+            await db.SaveChangesAsync();
+            return TypedResults.Created($"/api/Category/{category.Id}",category);
+        })
+        .WithName("CreateCategory")
+        .WithOpenApi();
+
+        group.MapDelete("/{id}", async Task<Results<Ok, NotFound>> (int id, Context db) =>
+        {
+            var affected = await db.Category
+                .Where(model => model.Id == id)
+                .ExecuteDeleteAsync();
+            return affected == 1 ? TypedResults.Ok() : TypedResults.NotFound();
+        })
+        .WithName("DeleteCategory")
+        .WithOpenApi();
+    }
+}
